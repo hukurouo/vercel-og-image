4da778bb9ecaf67ba333224f917d64b2304bb06a
hukurouo <owlbook248@gmail.com>
2022-01-15 15:29:06 +0900

    add wait

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -17,6 +17,7 @@ export async function getScreenshot(html: string, type: FileType, isDev: boolean
     const page = await getPage(isDev);
     await page.setViewport({ width: 1200, height: 630 });
     await page.setContent(html);
+    await page.waitForTimeout(1000);
     const file = await page.screenshot({ type });
     return file;
 }
